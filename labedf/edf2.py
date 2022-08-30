c51db75c8c2e653469fa06dad7cb7675f4d3e6be
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-08-30 20:42:31 +0900

    add preprocessing_annsignals_func option to split_annotations_edf2hdf function preprocessing_annsignals_func : Preprocess the signals split by annotations. ndarray : ch × annotation range

--- labedf/edf2.py
+++ labedf/edf2.py
@@ -3,8 +3,14 @@ import pyedflib
 from .utilities import edf
 import numpy as np
 import h5py
-def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = None,is_groupby:bool = False):
+from typing import Callable
+def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = None,is_groupby:bool = False,preprocessing_annsignals_func:Callable[[np.ndarray],np.ndarray] = None):
     """Split the edf file by annotation and save it in the hdf file.
+     Args:
+        edf_path : read edf path
+        export_path : write hdf path
+        filters : annotation filters
+        preprocessing_annsignals_func : Preprocess the signals split by annotations. ndarray : ch × annotation range 
     """
     with pyedflib.EdfReader(edf_path) as edf_reader:
         signals = np.array(edf.get_all_signals(edf_reader))
@@ -15,6 +21,8 @@ def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = N
             split_signals.append((ann_name,signals[:,ann_idx:(annotations[idx + 1][3] if idx + 1 < len(annotations) else signals.shape[1])]))
         if not(filters is None):
             split_signals =  [ss for ss in split_signals if ss[0] in filters]
+        if not(preprocessing_annsignals_func is None):
+            split_signals = [(ann_name,preprocessing_annsignals_func(ann_signals)) for ann_name,ann_signals in split_signals]
     with h5py.File(export_path, mode='w') as f:
         ann_group = f.create_group("/annotations")
         if is_groupby:
