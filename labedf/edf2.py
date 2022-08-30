4e22849e61d379f7321f5746e3420e23d3918ffe
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-08-30 19:44:01 +0900

    add is_groupby option to split_annotations_edf2hdf function

--- labedf/edf2.py
+++ labedf/edf2.py
@@ -3,7 +3,7 @@ import pyedflib
 from .utilities import edf
 import numpy as np
 import h5py
-def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = None):
+def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = None,is_groupby:bool = False):
     """Split the edf file by annotation and save it in the hdf file.
     """
     with pyedflib.EdfReader(edf_path) as edf_reader:
@@ -16,8 +16,22 @@ def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = N
         if not(filters is None):
             split_signals =  [ss for ss in split_signals if ss[0] in filters]
     with h5py.File(export_path, mode='w') as f:
-       ann_group = f.create_group("/annotations")
-       for idx,ann in enumerate(split_signals):
-            ann_name,ann_signals = ann
-            ann_group.create_dataset(f"{idx}.{ann_name}",ann_signals.shape,data=ann_signals)
+        ann_group = f.create_group("/annotations")
+        if is_groupby:
+            counts = {}
+            for idx,ann in enumerate(split_signals):
+                ann_name,ann_signals = ann
+                j = 0
+                if ann_name in counts:
+                    j = counts[ann_name] + 1
+                    counts[ann_name] = j
+                else:
+                    counts[ann_name] = 0
+
+                local_group = ann_group.require_group("./" + ann_name)
+                local_group.create_dataset(f"{j}",ann_signals.shape,data=ann_signals)
+        else:
+            for idx,ann in enumerate(split_signals):
+                ann_name,ann_signals = ann
+                ann_group.create_dataset(f"{idx}.{ann_name}",ann_signals.shape,data=ann_signals)
 # %%
