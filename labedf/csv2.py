0bb0f0469af2185a3e2de8c23362460ca6d181e9
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-09-27 19:58:09 +0900

    changed to write labels to hdf file attributes

--- labedf/csv2.py
+++ labedf/csv2.py
@@ -4,13 +4,15 @@ import labcsv
 from labcsv import DefaultHeaderName as DHName
 from .utilities import edf
 import numpy as np
-def merge_csv2edf(edf_path:str,csv_path:str,export_path:str = None,marker_name:str = "Marker"):
+def merge_csv2edf(edf_path:str,csv_path:str,export_path:str = None,marker_name:str = "Marker",label_header_name:str = None):
     """
     Write the lab.js sender names to edf as annotations. (Generate a copy file)
     Args:
         edf_path (str): edf file path
         csv_path (str): csv file path
         export_path (str, optional): output file path. Defaults to None.(None is <edf_path + "-copy">)
+        marker_name(str?) : filter sender name(= "sender" value)
+        label_header_name(str?) : label header name
     """
     edf_dir_path = os.path.dirname(edf_path)
     edf_filename_path =  os.path.splitext(os.path.basename(edf_path))[0]
@@ -26,7 +28,11 @@ def merge_csv2edf(edf_path:str,csv_path:str,export_path:str = None,marker_name:s
     marker_indexes = np.where(senders == marker_name)[0]
     time_runs = rlab.get_column_values(DHName.TIME_RUN)[marker_indexes]
     offset_times = ((time_runs - start_time_end) / 1000.0) + annos[1][1]
+    labels = rlab.get_column_values(label_header_name)[marker_indexes] if not(label_header_name is None) else [None for _ in range(len(marker_indexes))]
     def copied_func(redf:pyedflib.EdfReader,wedf:pyedflib.EdfWriter):
-        for ot in offset_times:
-            wedf.writeAnnotation(ot,-1,marker_name)
+        for ot,l in zip(offset_times,labels):
+            mn = marker_name
+            if not(l is None):
+                mn += f"_{l}"
+            wedf.writeAnnotation(ot,-1,mn)
     edf.copy(edf_reader,export_path,copied_func)
--- labedf/edf2.py
+++ labedf/edf2.py
@@ -15,31 +15,36 @@ def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = N
     with pyedflib.EdfReader(edf_path) as edf_reader:
         signals = np.array(edf.get_all_signals(edf_reader))
         annotations =  edf.get_annotations(edf_reader)
-        split_signals = []
+        split_signals = [] #[(annotation name,signal,label)]
         for idx,ann in enumerate(annotations):
             ann_name,ann_time,_,ann_idx = ann
-            split_signals.append((ann_name,signals[:,ann_idx:(annotations[idx + 1][3] if idx + 1 < len(annotations) else signals.shape[1])]))
+            split_ann_name = ann_name.split("_")
+            ann_group_name = "_".join(split_ann_name[:-1]) if len(split_ann_name) > 1 else ann_name
+            label= split_ann_name[-1] if len(split_ann_name) > 1 else ""
+            split_signals.append((ann_group_name,signals[:,ann_idx:(annotations[idx + 1][3] if idx + 1 < len(annotations) else signals.shape[1])],label))
         if not(filters is None):
             split_signals =  [ss for ss in split_signals if ss[0] in filters]
         if not(preprocessing_annsignals_func is None):
-            split_signals = [(ann_name,preprocessing_annsignals_func(ann_signals)) for ann_name,ann_signals in split_signals]
+            split_signals = [(ann_name,preprocessing_annsignals_func(ann_signals),label) for ann_name,ann_signals,label in split_signals]
     with h5py.File(export_path, mode='w') as f:
         ann_group = f.create_group("/annotations")
         if is_groupby:
             counts = {}
             for idx,ann in enumerate(split_signals):
-                ann_name,ann_signals = ann
+                ann_group_name,ann_signals,label = ann
                 j = 0
-                if ann_name in counts:
-                    j = counts[ann_name] + 1
-                    counts[ann_name] = j
+                if ann_group_name in counts:
+                    j = counts[ann_group_name] + 1
+                    counts[ann_group_name] = j
                 else:
-                    counts[ann_name] = 0
+                    counts[ann_group_name] = 0
 
-                local_group = ann_group.require_group("./" + ann_name)
-                local_group.create_dataset(f"{j}",ann_signals.shape,data=ann_signals)
+                local_group = ann_group.require_group("./" + ann_group_name)
+                d = local_group.create_dataset(f"{j}",ann_signals.shape,data=ann_signals)
+                d.attrs["label"] = label
         else:
             for idx,ann in enumerate(split_signals):
-                ann_name,ann_signals = ann
-                ann_group.create_dataset(f"{idx}.{ann_name}",ann_signals.shape,data=ann_signals)
+                ann_group_name,ann_signals,label = ann
+                d = ann_group.create_dataset(f"{idx}.{ann_group_name}",ann_signals.shape,data=ann_signals)
+                d.attrs["label"] = label
 # %%
