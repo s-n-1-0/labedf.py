65c4ad6aab6deb13c31d76405f90280394f0815f
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-09-30 00:19:33 +0900

    add overwrite option to split_annotations_edf2hdf function

--- labedf/edf2.py
+++ labedf/edf2.py
@@ -4,11 +4,13 @@ from .utilities import edf
 import numpy as np
 import h5py
 from typing import Callable
-def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = None,is_groupby:bool = False,preprocessing_annsignals_func:Callable[[np.ndarray],np.ndarray] = None):
+def split_annotations_edf2hdf(edf_path:str,export_path:str,is_overwrite:bool = False,is_groupby:bool = False,filters:list[str] = None,preprocessing_annsignals_func:Callable[[np.ndarray],np.ndarray] = None):
     """Split the edf file by annotation and save it in the hdf file.
      Args:
         edf_path : read edf path
         export_path : write hdf path
+        is_overwrite : overwrite the edf file
+        is_groupby : grouping
         filters : annotation filters
         preprocessing_annsignals_func : Preprocess the signals split by annotations. ndarray : ch × annotation range 
     """
@@ -26,22 +28,18 @@ def split_annotations_edf2hdf(edf_path:str,export_path:str,filters:list[str] = N
             split_signals =  [ss for ss in split_signals if ss[0] in filters]
         if not(preprocessing_annsignals_func is None):
             split_signals = [(ann_name,preprocessing_annsignals_func(ann_signals),label) for ann_name,ann_signals,label in split_signals]
-    with h5py.File(export_path, mode='w') as f:
-        ann_group = f.create_group("/annotations")
+    with h5py.File(export_path, mode='r+' if is_overwrite else 'w') as f:
+        ann_group = f.require_group("/annotations")
         if is_groupby:
-            counts = {}
             for idx,ann in enumerate(split_signals):
                 ann_group_name,ann_signals,label = ann
-                j = 0
-                if ann_group_name in counts:
-                    j = counts[ann_group_name] + 1
-                    counts[ann_group_name] = j
-                else:
-                    counts[ann_group_name] = 0
-
                 local_group = ann_group.require_group("./" + ann_group_name)
-                d = local_group.create_dataset(f"{j}",ann_signals.shape,data=ann_signals)
+                counter = local_group.attrs.get("count")
+                counter = 0 if counter is None else counter + 1
+                local_group.attrs["count"] = counter
+                d = local_group.create_dataset(f"{counter}",ann_signals.shape,data=ann_signals)
                 d.attrs["label"] = label
+
         else:
             for idx,ann in enumerate(split_signals):
                 ann_group_name,ann_signals,label = ann
