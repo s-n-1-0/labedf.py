9e6eb3899c33b86a5292043f54eee33746914565
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-10-31 16:21:23 +0900

    fix to consider sync_marker_name when merging (csv2.merge_csv2edf) * sync lag support

--- labedf/csv2.py
+++ labedf/csv2.py
@@ -4,8 +4,7 @@ import labcsv
 from labcsv import DefaultHeaderName as DHName
 from typing import Optional
 from .utilities import edf
-import numpy as np
-def merge_csv2edf(edf_path:str,csv_path:str,export_path:Optional[str] = None,marker_name:str = "Marker",end_marker_name:Optional[str]="__End__",end_marker_offset:float = 0,label_header_name:str = None):
+def merge_csv2edf(edf_path:str,csv_path:str,export_path:Optional[str] = None,marker_name:str = "Marker",sync_marker_name:str = "sync",end_marker_name:Optional[str]="__End__",end_marker_offset:float = 0,label_header_name:str = None):
     """
     Write the lab.js sender names to edf as annotations. (Generate a copy file)
     Args:
@@ -13,6 +12,7 @@ def merge_csv2edf(edf_path:str,csv_path:str,export_path:Optional[str] = None,mar
         csv_path (str): csv file path
         export_path (str?): output file path. Defaults to None.(None is <edf_path + "-copy">)
         marker_name(str) : filter sender name(= "sender" value)
+        sync_marker_name(str) : "response" value to synchronize files (None = 0 index)
         end_marker_name(str?) : annotation of marker_name end time
         end_marker_offset(float) : marker_name end time offset (seconds)
         label_header_name(str?) : label header name
@@ -22,21 +22,31 @@ def merge_csv2edf(edf_path:str,csv_path:str,export_path:Optional[str] = None,mar
     if export_path is None:
         export_path = f"{edf_dir_path}/{edf_filename_path}_copy.edf"
     edf_reader = pyedflib.EdfReader(edf_path)
-
-    #make lab.js annotaions
-    annos = edf.get_annotations(edf_reader)
     rlab_dtype = {label_header_name:str} if not (label_header_name is None) else None
     rlab = labcsv.read_csv(csv_path,dtype=rlab_dtype)
-    start_time_end = rlab.get_column_values(DHName.TIME_END)[0]
-    senders = rlab.get_column_values(DHName.SENDER)
-    marker_indexes = np.where(senders == marker_name)[0]
-    time_ends = rlab.get_column_values(DHName.TIME_END)[marker_indexes]
-    time_runs = rlab.get_column_values(DHName.TIME_RUN)[marker_indexes]
-    offset_time_runs = ((time_runs - start_time_end) / 1000.0) + annos[1][1]
-    offset_time_ends = ((time_ends - start_time_end) / 1000.0) + annos[1][1]
-    labels = rlab.get_column_values(label_header_name)[marker_indexes] if not(label_header_name is None) else [None for _ in range(len(marker_indexes))]
-    def copied_func(redf:pyedflib.EdfReader,wedf:pyedflib.EdfWriter):
-        for otr,ote,l in zip(offset_time_runs,offset_time_ends,labels):
+
+    edf_annos = edf.get_annotations(edf_reader)
+    senders,responses,time_ends,time_runs = rlab.get_column_list([DHName.SENDER,DHName.RESPONSE,DHName.TIME_END,DHName.TIME_RUN])
+    labels = rlab.get_column_values(label_header_name) if not(label_header_name is None) else [None] * len(responses)
+    sync_edf_annos = [ea for ea in edf_annos if ea[0] == sync_marker_name]
+    sync_lab_annos_indexes = [i for i,r in enumerate(list(responses)) if r == sync_marker_name ]
+    if len(sync_edf_annos) != len(sync_lab_annos_indexes):
+        raise Exception("Number of sync_marker_name in edf and csv files do not match")
+    start_time_count:int = -1
+    start_time_end:float = None
+    results:list[tuple[str,float,float]] = []
+    for label,sender,res, time_end,time_run in zip(labels,senders,responses,time_ends,time_runs):
+        if res == sync_marker_name:
+            start_time_count += 1
+            start_time_end = time_ends[sync_lab_annos_indexes[start_time_count]]
+        if sender != marker_name:
+            continue
+        offset_time_run = ((time_run - start_time_end) / 1000.0) + sync_edf_annos[start_time_count][1]
+        offset_time_end = ((time_end - start_time_end) / 1000.0) + sync_edf_annos[start_time_count][1]
+        results.append((label,offset_time_run,offset_time_end))
+
+    def copied_func(_ ,wedf:pyedflib.EdfWriter):
+        for l,otr,ote, in results:
             mn = marker_name
             if not(l is None):
                 mn += f"_{l}"
