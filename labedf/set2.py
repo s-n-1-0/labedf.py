d545f3990cc0651e4c2b86f9be8064f1bde9a5c4
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-01-24 18:17:26 +0900

    add "preprocessing_func" option (set2.merge_set2hdf)

--- labedf/set2.py
+++ labedf/set2.py
@@ -1,4 +1,6 @@
 # %%
+from typing import Callable
+import numpy as np
 import mne
 import h5py
 
@@ -7,13 +9,15 @@ def merge_set2hdf(edf_path:str,
     labels:list[str],
     marker_names:list[str] = ["Marker"],
     is_overwrite:bool = False,
-    is_groupby:bool = False):
+    is_groupby:bool = False,
+    preprocessing_func:Callable[[np.ndarray,str],np.ndarray] = None):
     """Split the edf file by annotation and save it in the hdf file.
      Args:
         edf_path : read edf path
         export_path : write hdf path
         is_overwrite : overwrite the edf file
         is_groupby : grouping
+        preprocessing_func(function[[signals,label],ndarray]?) : Processes each segmented data. ndarray : ch × annotation range
     """
 
     epochs = mne.io.read_epochs_eeglab(edf_path)
@@ -21,18 +25,20 @@ def merge_set2hdf(edf_path:str,
         def write_hdf(marker_name:str,label:str):
             data = epochs.get_data(item=f"{marker_name}__{label}")
             ann_group = f.require_group("/annotations")
-            if is_groupby:
-                for idx in range(data.shape[0]):
-                    local_group = ann_group.require_group("./" + marker_name)
-                    counter = local_group.attrs.get("count")
-                    counter = 0 if counter is None else counter + 1
-                    local_group.attrs["count"] = counter
-                    d = local_group.create_dataset(f"{counter}",data[idx,:,:].shape,data=data[idx,:,:])
-                    d.attrs["label"] = label
-            else:
-                for idx in range(data.shape[0]):
-                    d = ann_group.create_dataset(f"{idx}.{marker_name}",data[idx,:,:].shape,data=data[idx,:,:])
-                    d.attrs["label"] = label
+            for idx in range(data.shape[0]):
+                data_ch = data[idx,:,:]
+                if not(preprocessing_func is None):
+                    data_ch = preprocessing_func(data_ch,label)
+                if is_groupby:
+                        local_group = ann_group.require_group("./" + marker_name)
+                        counter = local_group.attrs.get("count")
+                        counter = 0 if counter is None else counter + 1
+                        local_group.attrs["count"] = counter
+                        d = local_group.create_dataset(f"{counter}",data_ch.shape,data=data_ch)
+                        d.attrs["label"] = label
+                else:
+                        d = ann_group.create_dataset(f"{idx}.{marker_name}",data_ch.shape,data=data_ch)
+                        d.attrs["label"] = label
         for marker_name in marker_names:
             for label in labels:
                 write_hdf(marker_name,label)
